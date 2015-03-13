263c996e02d0a50f51e48b2675df1e50d3be0ba8
edmundas <edmundas.ramanauskas@gmail.com>
2015-03-13 22:48:35 +0200

    minor fix

--- src/main/java/com/marketplace/app/services/impl/ApplicationServiceImpl.java
+++ src/main/java/com/marketplace/app/services/impl/ApplicationServiceImpl.java
@@ -3,22 +3,17 @@ package com.marketplace.app.services.impl;
 import com.marketplace.app.domain.entities.Application;
 import com.marketplace.app.domain.repositories.ApplicationRepository;
 import com.marketplace.app.services.ApplicationService;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author edmundas
  */
-@Repository
 public class ApplicationServiceImpl implements ApplicationService {
     
-    @Autowired
+    @Resource
     private ApplicationRepository applicationRepository;
     
     @Override
